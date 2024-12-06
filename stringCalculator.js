8df9c4b734807adfbca691407a5084a3bc4943a4
Ashi2507 <110546125+Ashi2507@users.noreply.github.com>
2024-12-06 14:26:30 +0530

    Handle Any Number of Numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -6,3 +6,4 @@ function add(numbers) {
 }
 
 module.exports = add;
+
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -11,3 +11,7 @@ test('should return the number itself for a single number', () => {
 test('should return the sum of two numbers', () => {
   expect(add("1,5")).toBe(6);
 });
+test('should return the sum of multiple numbers', () => {
+  expect(add("1,2,3")).toBe(6);
+  expect(add("4,5,6,7")).toBe(22);
+});
